677c6cb6438b2de76dfd114bd1bbdb8512dffaec
Addi3 <addiec787@gmail.com>
2025-03-12 18:17:53 +0000

    fix: engine_block texture pos fix: plinth interior portal height

--- src/main/java/dev/amble/ait/data/schema/exterior/variant/plinth/PlinthVariant.java
+++ src/main/java/dev/amble/ait/data/schema/exterior/variant/plinth/PlinthVariant.java
@@ -40,21 +40,21 @@ public abstract class PlinthVariant extends ExteriorVariantSchema {
     @Override
     public Vec3d adjustPortalPos(Vec3d pos, byte direction) {
         return switch (direction) {
-            case 0 -> pos.add(0, 0.4875, -0.4); // NORTH
-            case 1, 2, 3 -> pos.add(0.35f, 0.4875, -0.35f); // NORTH EAST p n
-            case 4 -> pos.add(0.2, 0.4875, 0); // EAST
-            case 5, 6, 7 -> pos.add(0.35f, 0.4875, 0.35f); // SOUTH EAST p p
-            case 8 -> pos.add(0, 0.4875, 0.4); // SOUTH
-            case 9, 10, 11 -> pos.add(-0.35f, 0.4875, 0.35f); // SOUTH WEST n p
-            case 12 -> pos.add(-0.4, 0.4875, 0); // WEST
-            case 13, 14, 15 -> pos.add(-0.35f, 0.4875, -0.35f); // NORTH WEST n n
+            case 0 -> pos.add(0, -0.3, -0.4); // NORTH
+            case 1, 2, 3 -> pos.add(0.35f, -0.3, -0.35f); // NORTH EAST p n
+            case 4 -> pos.add(0.2, -0.3, 0); // EAST
+            case 5, 6, 7 -> pos.add(0.35f, -0.3, 0.35f); // SOUTH EAST p p
+            case 8 -> pos.add(0, -0.3, 0.4); // SOUTH
+            case 9, 10, 11 -> pos.add(-0.35f, -0.3, 0.35f); // SOUTH WEST n p
+            case 12 -> pos.add(-0.4, -0.3, 0); // WEST
+            case 13, 14, 15 -> pos.add(-0.35f, -0.3, -0.35f); // NORTH WEST n n
             default -> pos;
         };
     }
 
     @Override
     public double portalHeight() {
-        return 4.3d;
+        return 2.8d;
     }
 
     @Override

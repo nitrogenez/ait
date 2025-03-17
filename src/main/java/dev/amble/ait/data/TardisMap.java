48d3c95a662b51355bb7aa972f7bb0e18d632b75
Dr Theo <71725814+DrTheodor@users.noreply.github.com>
2025-03-17 22:36:53 +0300

    fix: attempt fixing the sync crash

--- src/main/java/dev/amble/ait/data/TardisMap.java
+++ src/main/java/dev/amble/ait/data/TardisMap.java
@@ -2,10 +2,11 @@ package dev.amble.ait.data;
 
 import java.util.HashMap;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 import dev.amble.ait.core.tardis.Tardis;
 
-public class TardisMap<T extends Tardis> extends HashMap<UUID, T> {
+public class TardisMap<T extends Tardis> extends ConcurrentHashMap<UUID, T> {
 
     public T put(T t) {
         return this.put(t.getUuid(), t);

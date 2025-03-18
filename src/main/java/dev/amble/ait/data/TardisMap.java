d849de41b687173d1cf813304505468032d0f760
Dr Theo <71725814+DrTheodor@users.noreply.github.com>
2025-03-18 09:28:07 +0300

    fix: return null when the key is null

--- src/main/java/dev/amble/ait/data/TardisMap.java
+++ src/main/java/dev/amble/ait/data/TardisMap.java
@@ -1,12 +1,20 @@
 package dev.amble.ait.data;
 
-import java.util.HashMap;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 import dev.amble.ait.core.tardis.Tardis;
+import org.jetbrains.annotations.Nullable;
 
 public class TardisMap<T extends Tardis> extends ConcurrentHashMap<UUID, T> {
+
+    @Nullable
+    public T get(UUID id) {
+        if (id == null)
+            return null;
+
+        return super.get(id);
+    }
 
     public T put(T t) {
         return this.put(t.getUuid(), t);

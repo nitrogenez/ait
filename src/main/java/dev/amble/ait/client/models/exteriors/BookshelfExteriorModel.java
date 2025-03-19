b43241c9cb633034888311430c82a38443771595
Addi3 <addiec787@gmail.com>
2025-03-19 18:40:15 +0000

    fix: BOTI portal poses (I NEED GEOMETRIC DOOR MODEL! @loqor)

--- src/main/java/dev/amble/ait/client/models/exteriors/BookshelfExteriorModel.java
+++ src/main/java/dev/amble/ait/client/models/exteriors/BookshelfExteriorModel.java
@@ -208,13 +208,13 @@ public class BookshelfExteriorModel extends ExteriorModel {
             this.bookshelf.getChild("right_door").yaw = (float) Math.toRadians(maxRot * door.getRightRot());
         }
 
-//        if (isBOTI) {
-//            matrices.push();
-//            matrices.scale(1F, 1F, 1F);
-//            matrices.translate(0.04, -1.26f, -0.36);
-//            this.bookshelf.getChild("left_door").render(matrices, vertices, light, overlay, red, green, blue, pAlpha);
-//            this.bookshelf.getChild("right_door").render(matrices, vertices, light, overlay, red, green, blue, pAlpha);
-//            matrices.pop();
-//        }
+      if (isBOTI) {
+          matrices.push();
+            matrices.scale(1F, 1F, 1F);
+           matrices.translate(0.04, -1.26f, -0.36);
+           this.bookshelf.getChild("left_door").render(matrices, vertices, light, overlay, red, green, blue, pAlpha);
+           this.bookshelf.getChild("right_door").render(matrices, vertices, light, overlay, red, green, blue, pAlpha);
+            matrices.pop();
+        }
     }
 }
--- src/main/java/dev/amble/ait/data/schema/exterior/variant/easter_head/EasterHeadVariant.java
+++ src/main/java/dev/amble/ait/data/schema/exterior/variant/easter_head/EasterHeadVariant.java
@@ -39,20 +39,20 @@ public abstract class EasterHeadVariant extends ExteriorVariantSchema {
         return false;
     }
 
-//    @Override
-//    public Vec3d adjustPortalPos(Vec3d pos, byte direction) {
-//        return switch (direction) {
-//            case 0 -> pos.add(0, 0.27, -0.025); // NORTH
-//            case 1, 2, 3 -> pos.add(0.025, 0.27, -0.025); // NORTH EAST p n
-//            case 4 -> pos.add(0.5, 0.27, 0); // EAST
-//            case 5, 6, 7 -> pos.add(0.025, 0.27, 0.025); // SOUTH EAST p p
-//            case 8 -> pos.add(0, 0.27, 0.5); // SOUTH
-//            case 9, 10, 11 -> pos.add(-0.025, 0.27, 0.025); // SOUTH WEST n p
-//            case 12 -> pos.add(-0.5, 0.27, 0); // WEST
-//            case 13, 14, 15 -> pos.add(-0.025, 0.27, -0.025); // NORTH WEST n n
-//            default -> pos;
-//        };
-//    }
+    @Override
+   public Vec3d adjustPortalPos(Vec3d pos, byte direction) {
+        return switch (direction) {
+            case 0 -> pos.add(0, 0.27, -0.025); // NORTH
+            case 1, 2, 3 -> pos.add(0.025, 0.27, -0.025); // NORTH EAST p n
+            case 4 -> pos.add(0.5, 0.27, 0); // EAST
+           case 5, 6, 7 -> pos.add(0.025, 0.27, 0.025); // SOUTH EAST p p
+            case 8 -> pos.add(0, 0.27, 0.5); // SOUTH
+            case 9, 10, 11 -> pos.add(-0.025, 0.27, 0.025); // SOUTH WEST n p
+            case 12 -> pos.add(-0.5, 0.27, 0); // WEST
+            case 13, 14, 15 -> pos.add(-0.025, 0.27, -0.025); // NORTH WEST n n
+           default -> pos;
+        };
+    }
 
     @Override
     public Vec3d seatTranslations() {

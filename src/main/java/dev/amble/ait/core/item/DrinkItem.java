c172a42dcfe362969c2ca1025ea8813a74a10eee
MaketendoDev <maketendomail@gmail.com>
2025-03-21 21:26:16 +0000

    fix: empty mug is drinkable feat: made milk clear status effects

--- src/main/java/dev/amble/ait/core/item/DrinkItem.java
+++ src/main/java/dev/amble/ait/core/item/DrinkItem.java
@@ -23,12 +23,16 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.text.Text;
-import net.minecraft.util.*;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 
+import dev.amble.ait.AITMod;
 import dev.amble.ait.core.AITItems;
 import dev.amble.ait.core.drinks.Drink;
 import dev.amble.ait.core.drinks.DrinkRegistry;
@@ -57,11 +61,15 @@ public class DrinkItem extends Item {
 
     @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+
         PlayerEntity playerEntity = user instanceof PlayerEntity ? (PlayerEntity)user : null;
         if (playerEntity instanceof ServerPlayerEntity) {
             Criteria.CONSUME_ITEM.trigger((ServerPlayerEntity)playerEntity, stack);
         }
         if (!world.isClient) {
+
+            if (DrinkUtil.getDrink(stack) == DrinkRegistry.getInstance().get(AITMod.id("milk"))) user.clearStatusEffects();
+
             List<StatusEffectInstance> list = DrinkUtil.getDrinkEffects(stack);
             for (StatusEffectInstance statusEffectInstance : list) {
                 if (statusEffectInstance.getEffectType().isInstant()) {
@@ -70,6 +78,7 @@ public class DrinkItem extends Item {
                 }
                 user.addStatusEffect(new StatusEffectInstance(statusEffectInstance));
             }
+
         }
         if (playerEntity != null) {
             playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
@@ -96,6 +105,7 @@ public class DrinkItem extends Item {
         PlayerEntity playerEntity = context.getPlayer();
         ItemStack itemStack = context.getStack();
         BlockState blockState = world.getBlockState(blockPos);
+
         if (context.getSide() != Direction.DOWN && blockState.isIn(BlockTags.CONVERTABLE_TO_MUD) && DrinkUtil.getDrink(itemStack) == DrinkRegistry.EMPTY_MUG) {
             world.playSound(null, blockPos, SoundEvents.ENTITY_GENERIC_SPLASH, SoundCategory.BLOCKS, 1.0f, 1.0f);
             playerEntity.setStackInHand(context.getHand(), ItemUsage.exchangeStack(itemStack, playerEntity, new ItemStack(AITItems.MUG)));
@@ -126,8 +136,16 @@ public class DrinkItem extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        return ItemUsage.consumeHeldItem(world, user, hand);
+        ItemStack stack = user.getStackInHand(hand);
+
+        if (DrinkUtil.getDrink(stack) != DrinkRegistry.getInstance().get(AITMod.id("mug_empty"))) {
+            return ItemUsage.consumeHeldItem(world, user, hand);
+        }
+
+        return TypedActionResult.fail(stack);
     }
+
+
 
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {

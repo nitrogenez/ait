17d1a2cbbcff7185289e916adb6e80fa04e7a308
Loqor <65447472+Loqor@users.noreply.github.com>
2025-03-31 17:40:49 -0500

    fix: rotations are correct now hopefully

--- src/main/java/dev/amble/ait/client/animation/AnimationConstants.java
+++ src/main/java/dev/amble/ait/client/animation/AnimationConstants.java
@@ -12,4 +12,26 @@ public class AnimationConstants {
         Vector3f vector3f2 = keyframes[end].target();
         return vector3f.lerp(vector3f2, 0, destination).mul(scale);
     };
+
+    // Used to fix the BEZIER interpolation type for Blockbench's keyframes
+    public static final Transformation.Interpolation BEZIER = (destination, delta, keyframes, start, end, scale) -> {
+        Vector3f p0 = keyframes[start].target();
+        Vector3f p1 = keyframes[start + 1].target();
+        Vector3f p2 = keyframes[end - 1].target();
+        Vector3f p3 = keyframes[end].target();
+
+        float u = 1 - delta;
+        float tt = delta * delta;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * delta;
+
+        Vector3f result = new Vector3f();
+        result.add(p0.mul(uuu));
+        result.add(p1.mul(3 * uu * delta));
+        result.add(p2.mul(3 * u * tt));
+        result.add(p3.mul(ttt));
+
+        return result.mul(scale);
+    };
 }

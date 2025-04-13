37e336ab7845d936686c683ec65e92951fda769a
James Hall <73184526+duzos@users.noreply.github.com>
2025-04-13 22:00:41 +0100

    fix missing newline

--- scripts/generate_changelogs.py
+++ scripts/generate_changelogs.py
@@ -67,7 +67,7 @@ for e in j:
 
     if ':cl:' not in body:
         print(f'#{num}: changelog not found, using title')
-        clines.append(e['title'] + f' ([#{num}]({e['html_url']})')
+        clines.append(e['title'] + f' ([#{num}]({e['html_url']}))\n')
         continue
 
     print(f'#{num} changelog found')
@@ -82,7 +82,7 @@ for e in j:
         if ls[n] in STOP:
             break
 
-        clines.append(ls[n] +f' ([#{num}]({e['html_url']})')
+        clines.append(ls[n] +f' ([#{num}]({e['html_url']}))')
 
 with open('CHANGELOG.md', 'w') as f:
     f.write(PREFIX + str(last_update))

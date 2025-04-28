de986bbd8ccffb153a2dfc31a823d763ee87a466
Dr Theo <71725814+DrTheodor@users.noreply.github.com>
2025-04-28 22:17:47 +0300

    make control entities regular entities

--- src/main/java/dev/amble/ait/core/item/control/ControlBlockItem.java
+++ src/main/java/dev/amble/ait/core/item/control/ControlBlockItem.java
@@ -1,25 +1,18 @@
 package dev.amble.ait.core.item.control;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.jetbrains.annotations.Nullable;
-
 import net.minecraft.block.Block;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-import dev.amble.ait.core.entities.ConsoleControlEntity;
+import java.util.List;
+import java.util.Optional;
 
 public abstract class ControlBlockItem extends BlockItem {
     public static final String CONTROL_ID_KEY = "controlId";
@@ -27,18 +20,6 @@ public abstract class ControlBlockItem extends BlockItem {
 
     protected ControlBlockItem(Block block, Settings settings) {
         super(block, settings);
-    }
-
-    @Override
-    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
-        if (!user.getWorld().isClient() && entity instanceof ConsoleControlEntity ce) {
-            NbtCompound nbt = stack.getOrCreateNbt();
-            nbt.putString(CONTROL_ID_KEY, ce.getControl().id().toString());
-            ce.getConsole().ifPresent(be -> nbt.putString(CONSOLE_TYPE_ID_KEY, be.getTypeSchema().id().toString()));
-            return ActionResult.SUCCESS;
-        }
-
-        return super.useOnEntity(stack, user, entity, hand);
     }
 
     @Override
